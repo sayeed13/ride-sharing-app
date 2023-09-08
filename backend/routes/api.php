5bc06e6a5e79b8773fd11161f326e46eb59e7f8e
Sayeed <sksayed13@gmail.com>
2023-09-08 18:52:30 +0400

    update api for user

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -7,3 +7,11 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/login/verify', [LoginController::class, 'verify']);
 
+Route::group(["middlewere" => "auth:sanctum"], function(){
+
+    Route::get('/user', function() {
+        return $this->user();
+    });
+    
+});
+
