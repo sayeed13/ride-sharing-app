08e3e5de68f36d867e70ee3df9febde744f7bc93
Sayeed <sksayed13@gmail.com>
2023-09-09 21:00:18 +0400

    create more routes of trips
    trip accept, start, end, and location routes created.

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -21,5 +21,10 @@ Route::group(["middlewere" => "auth:sanctum"], function(){
     Route::post('/trip', [TripController::class, 'store']);
     Route::get('/trip/{trip}', [TripController::class, 'show']);
 
+    Route::post('/trip/{$trip}/accept', [TripController::class, 'accept']);
+    Route::post('/trip/{$trip}/start', [TripController::class, 'start']);
+    Route::post('/trip/{$trip}/end', [TripController::class, 'end']);
+    Route::post('/trip/{$trip}/location', [TripController::class, 'location']);
+
 });
 
